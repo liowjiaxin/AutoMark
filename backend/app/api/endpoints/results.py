887c6eb6ae5478aae7a19016711fbac70f435b6a
Wang Xue Rui <wangxuerui2003@gmail.com>
2025-03-10 11:19:02 +0800

    fix: get all results in a query instead of one

--- backend/app/api/endpoints/results.py
+++ backend/app/api/endpoints/results.py
@@ -10,18 +10,18 @@ router = APIRouter(prefix="/api")
 @router.get("/result/{student_id}")
 async def get_student_result(student_id: int, session: Session = Depends(get_db)):
     statement = select(Submission).where(Submission.student_id == student_id)
-    result = session.exec(statement).first()
-    if not result:
+    results = session.exec(statement).all()
+    if not results:
         raise HTTPException(
             status_code=404, detail=f"Result for student id {student_id} not found"
         )
 
-    return {"result": result}
+    return {"results": results}
 
 
 @router.get("/results/")
 async def get_results(session: Session = Depends(get_db)):
     # TODO: use query param to do pagination and sorting
     statement = select(Submission)
-    results_list = session.exec(statement).first()
+    results_list = session.exec(statement).all()
     return {"results": results_list}
