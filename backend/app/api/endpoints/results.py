153eafd4e021f75e7ea1bbfb235bb8e6aaefc0f8
Wang Xue Rui <wangxuerui2003@gmail.com>
2025-03-10 11:04:02 +0800

    feat: get result by student id and get all results

--- backend/app/api/endpoints/results.py
+++ backend/app/api/endpoints/results.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
 from typing import List
 from db.models import Submission
+from api.dependencies import get_db
+from sqlmodel import Session, select
 
 router = APIRouter(prefix="/api")
 
-@router.get("/results")
-def get_results(student_id: int = Query(None, description="ID of the student to filter results")):
-    # TODO: sort
-    results = Submission.filter(student_id=student_id).all()
-    if not results:
-        raise HTTPException(status_code=404, detail="Results not found")
 
-    return {"results": results}
+@router.get("/result/{student_id}")
+async def get_student_result(student_id: int, session: Session = Depends(get_db)):
+    statement = select(Submission).where(Submission.student_id == student_id)
+    result = session.exec(statement).first()
+    if not result:
+        raise HTTPException(
+            status_code=404, detail=f"Result for student id {student_id} not found"
+        )
+
+    return {"result": result}
+
+
+@router.get("/results/")
+async def get_results(session: Session = Depends(get_db)):
+    # TODO: use query param to do pagination and sorting
+    statement = select(Submission)
+    results_list = session.exec(statement).first()
+    return {"results": results_list}
